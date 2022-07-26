5a65e06abedf343bd9d343305e888d1094ecab39
Emerson Millard <emillard.code@gmail.com>
2022-07-26 12:00:00 -0700

    Added Zuul annotation to main class.

--- src/main/java/com/project/springcloudzuuljt3/SpringCloudZuulJt3Application.java
+++ src/main/java/com/project/springcloudzuuljt3/SpringCloudZuulJt3Application.java
@@ -2,12 +2,16 @@ package com.project.springcloudzuuljt3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringCloudZuulJt3Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudZuulJt3Application.class, args);
+
     }
 
 }
